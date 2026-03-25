f762be47521de4d78680897af58bf70a090b65f4
PuddinPopinski <wconrad91@hotmail.com>
2026-03-25 12:21:10 +1000

    Pass on the card visuals

--- DungeonKeeperRogue/Assets/Scripts/Gameplay/Cards/Card.cs
+++ DungeonKeeperRogue/Assets/Scripts/Gameplay/Cards/Card.cs
@@ -1,11 +1,26 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Card : MonoBehaviour
 {
 	[SerializeField] private CardType _cardType;
-	[SerializeField] private GameObject _highlight;
 	[SerializeField] private GameObject _prefabToSpawn;
+
+	[Header("Card info")] 
+	[SerializeField] private string _name;
+	[SerializeField] private string _description;
+	[SerializeField] private Sprite _icon;
+	
+	[Header("Card detail visuals")] 
+	[SerializeField] private TMP_Text _nameTextField;
+	[SerializeField] private TMP_Text _descriptionTextField;
+	[SerializeField] private SpriteRenderer _iconSpriteRenderer;
+
+	[Header("Events")]
+	[SerializeField] private UnityEvent _onHighlightedEvent;
+	[SerializeField] private UnityEvent _onStopHighlightedEvent;
 
 	private bool _isPlaying = false;
 	private Player _owner;
@@ -22,6 +37,10 @@ public class Card : MonoBehaviour
 	public void Init(Player owner)
 	{
 		_owner = owner;
+		
+		_nameTextField.SetText(_name);
+		_descriptionTextField.SetText(_description);
+		_iconSpriteRenderer.sprite = _icon;
 	}
 
 	public IEnumerator PlayCard()
@@ -42,6 +61,12 @@ public class Card : MonoBehaviour
 
 	public void SetHighlighted(bool highlight)
 	{
-		_highlight.SetActive(highlight);
+		if (highlight)
+		{
+			_onHighlightedEvent?.Invoke();
+			return;
+		}
+		
+		_onStopHighlightedEvent?.Invoke();
 	}
 }
